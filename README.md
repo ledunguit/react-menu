6698313d816f1384ee516ff9baacc9e6daf8cfd7
Lê Dũng <ledung.itsme@gmail.com>
2021-11-07 16:58:26 +0700

    Done

---
+++ README.md
@@ -0,0 +1,1 @@
+React project 05 - Food Menu
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      return setMenuItems(items);
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return (
+    <main>
+      <section className="menu seciton">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={allCategories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
+}
+
+export default App;
